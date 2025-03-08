b999dddf72e59ae3c798af8b777fa25c42ef5d19
HariPad2005 <hariharanbindu2019@gmail.com>
2025-03-08 17:35:54 +0530

    Basic Ethers.js Codes

--- defai/src/App.js
+++ defai/src/App.js
@@ -1,17 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { ethers } from "ethers";
-// A Web3Provider wraps a standard Web3 provider, which is
-// what MetaMask injects as window.ethereum into each page
-const provider = new ethers.BrowserProvider(window.ethereum);
+import { Contract } from "ethers";
 
-// MetaMask requires requesting permission to connect users accounts
-await provider.send("eth_requestAccounts", []);
+let signer = null;
 
-// The MetaMask plugin also allows signing transactions to
-// send ether and pay to change state within the blockchain.
-// For this, you need the account signer...
-const signer = provider.getSigner()
+let provider;
+if (window.ethereum == null) {
+
+    // If MetaMask is not installed, we use the default provider,
+    // which is backed by a variety of third-party services (such
+    // as INFURA). They do not have private keys installed,
+    // so they only have read-only access
+    console.log("MetaMask not installed; using read-only defaults")
+    provider = ethers.getDefaultProvider()
+
+} else {
+
+    // Connect to the MetaMask EIP-1193 object. This is a standard
+    // protocol that allows Ethers access to make all read-only
+    // requests through MetaMask.
+        provider = new ethers.BrowserProvider(window.ethereum)
+        signer = await provider.getSigner();
+        
+        const address = await signer.getAddress(); // Get user's wallet address
+
+        const b=await provider.getBlockNumber()
+      
+        const balanceWei = await provider.getBalance(address);
+        const balanceEther = ethers.formatEther(balanceWei); // Convert from Wei to Ether
+
+        console.log(`Your wallet address: ${address}`);
+        console.log(`Your balance: ${balanceEther} ETH`);
+        console.log(`Your block: ${b}`);
+    
+    // It also provides an opportunity to request access to write
+    // operations, which will be performed by the private key
+    // that MetaMask manages for the user.
+    
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -30,7 +59,52 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
 export default App;
+
+// // The contract ABI (fragments we care about)
+        // let abi = [
+        //   "function decimals() view returns (uint8)",
+        //   "function symbol() view returns (string)",
+        //   "function balanceOf(address a) view returns (uint)"
+        // ]
+        
+        // Create a contract; connected to a Provider, so it may
+        // only access read-only methods (like view and pure)
+        // let contract = new ethers.Contract("0x6B175474E89094C44Da98b954EedeAC495271d0F", abi, provider)
+        // const sym = await contract.symbol()
+        // // 'DAI'
+
+        // // The number of decimals the token uses
+        // let decimals = await contract.decimals()
+        // 18n
+
+        // Read the token balance for an account
+        // let balance = await contract.balanceOf(address);
+// 4000000000000000000000n
+
+    
+        // '4000.0'
+        // Fetch the balance
+        // let abi = [
+        //   "function transfer(address to, uint amount)"
+        // ]
+        
+        // // Connected to a Signer; can make state changing transactions,
+        // // which will cost the account ether
+        // let contract = new Contract("0x6B175474E89094C44Da98b954EedeAC495271d0F", abi, signer)
+        
+        // // Send 1 DAI
+        // let amount = ethers.parseUnits("1.0", 18);
+        
+        // // Send the transaction
+        // let tx = await contract.transfer(address, amount)
+        
+        // Currently the transaction has been sent to the mempool,
+        // but has not yet been included. So, we...
+        
+        // ...wait for the transaction to be included.
+        // await tx.wait()
